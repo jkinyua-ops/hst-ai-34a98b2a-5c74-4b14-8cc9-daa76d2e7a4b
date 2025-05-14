bb11830ce82a3805a339afc549dd70dca76784d3
Jeremy Kinyua <jkinyua-ops@outlook.com>
2025-05-14 11:17:33 +0300

    update file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,11 @@ export default {
 					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
 					border: 'hsl(var(--sidebar-border))',
 					ring: 'hsl(var(--sidebar-ring))'
-				}
+				},
+				teal: {
+					500: '#14b8a6',
+					200: '#99f6e4',
+				},
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
