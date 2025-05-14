d92461e6c39d4dad8cdf1d8802fdcc0357b0fd10
Jeremy Kinyua <jkinyua-ops@outlook.com>
2025-05-14 11:19:51 +0300

    update file

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <nav className="bg-teal-500 p-4">
+      <nav className="bg-green-500 p-4">
         <div className="container mx-auto flex justify-between items-center">
           <Link to="/" className="text-white text-2xl font-bold">Your App</Link>
           <div className="space-x-4">
-            <Link to="/" className="text-white hover:text-teal-200">Home</Link>
-            <Link to="/about" className="text-white hover:text-teal-200">About</Link>
-            <Link to="/contact" className="text-white hover:text-teal-200">Contact</Link>
+            <Link to="/" className="text-white hover:text-green-200">Home</Link>
+            <Link to="/about" className="text-white hover:text-green-200">About</Link>
+            <Link to="/contact" className="text-white hover:text-green-200">Contact</Link>
           </div>
         </div>
       </nav>
